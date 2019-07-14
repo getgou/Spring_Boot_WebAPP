d0b14b7a110515b1d7515eebeaac5f69c39c8e0b
Get <getgou@gmail.com>
2019-07-14 22:11:24 +0200

    Initial version

---
+++ ShoppingCartWebAPp/src/main/java/webapp/ShoppingCartWebAPpApplication.java
@@ -0,0 +1,13 @@
+package webapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShoppingCartWebAPpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShoppingCartWebAPpApplication.class, args);
+	}
+
+}
